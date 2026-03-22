c4629513223ed56b699d1821bece3347d4476122
Krzysztof Zaucha <krzysztof@zaucha.co.uk>
2026-03-22 17:53:24 +0000

    update scripts and gh actions

--- scripts/tools.sh
+++ scripts/tools.sh
@@ -19,7 +19,7 @@ case "$1" in
 
 "cmd")
   docker run --rm \
-    --user $(id -u):$(id -g) \
+    --user "$(id -u):$(id -g)" \
     --name "${BASE_NAME}-cmd" \
     -e DEBUG \
     -e GOCACHE="${PWD}/tmp" \
@@ -171,7 +171,7 @@ EOF
     --build-arg VENDOR \
     --build-arg BASE_IMAGE_VERSION=latest \
     --build-arg NAME="${BASE_NAME}" \
-    --build-arg VERSION=$(scripts/tools.sh version) \
+    --build-arg VERSION="$(scripts/tools.sh version)" \
     --build-arg BUILD_TIME="$(date -u +"%Y-%m-%dT%H:%M:%SZ")" \
     -t "${IMAGE_NAME}:latest" \
     -f Dockerfile .
