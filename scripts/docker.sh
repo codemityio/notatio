bee6afe1683f94cf871c7f5f63867b1c4c92e822
Krzysztof Zaucha <krzysztof@zaucha.co.uk>
2026-03-23 20:57:21 +0000

    udpate gh actions and docker build pipelines

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -9,6 +9,33 @@ fi
 
 case "$1" in
 
+"buildx")
+  docker buildx build \
+    --platform linux/amd64,linux/arm64 \
+    --target=final \
+    --build-arg VENDOR \
+    --build-arg BASE_IMAGE_VERSION=latest \
+    --build-arg NAME="${BASE_NAME}" \
+    --build-arg VERSION="$(scripts/tools.sh version)" \
+    --build-arg BUILD_TIME="$(date -u +"%Y-%m-%dT%H:%M:%SZ")" \
+    -t "${IMAGE_NAME}:latest" \
+    -t "${IMAGE_NAME}:$(scripts/tools.sh version)" \
+    --push \
+    -f Dockerfile .
+  ;;
+
+"build")
+  docker image build \
+    --target=final \
+    --build-arg VENDOR \
+    --build-arg BASE_IMAGE_VERSION=latest \
+    --build-arg NAME="${BASE_NAME}" \
+    --build-arg VERSION="$(scripts/tools.sh version)" \
+    --build-arg BUILD_TIME="$(date -u +"%Y-%m-%dT%H:%M:%SZ")" \
+    -t "${IMAGE_NAME}:latest" \
+    -f Dockerfile .
+  ;;
+
 "tag")
   docker tag "${IMAGE_NAME}":latest "${IMAGE_NAME}:$(scripts/tools.sh version)"
   ;;
